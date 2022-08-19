0364955cc4bbcf7b8e50f0e6bf4e8ca0e2707f60
Adam Burton <adz21c@gmail.com>
2022-08-19 22:30:08 +0100

    Github Workflows

--- src/Periturf.IdentityServer/Configuration/ClientStore.cs
+++ src/Periturf.IdentityServer/Configuration/ClientStore.cs
@@ -1,14 +1,23 @@
 ﻿using Duende.IdentityServer.Models;
 using Duende.IdentityServer.Stores;
+using System.Diagnostics.CodeAnalysis;
 using System.Threading.Tasks;
 
 namespace Periturf.IdentityServer.Configuration
 {
+    [ExcludeFromCodeCoverage]
     class ClientStore : IClientStore
     {
+        private readonly ConfigurationStore _store;
+
+        public ClientStore(ConfigurationStore store)
+        {
+            _store = store;
+        }
+
         public Task<Client> FindClientByIdAsync(string clientId)
         {
-            throw new System.NotImplementedException();
+            return _store.FindClientByIdAsync(clientId);
         }
     }
 }
